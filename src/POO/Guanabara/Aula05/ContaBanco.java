5cf77126f76bac0a622b4167e78bd3719ac85d5f
Larissa Martins <larissamartins.job@gmail.com>
2021-10-03 18:38:52 -0400

    Add getters and setters

--- src/POO/Guanabara/Aula05/ContaBanco.java
+++ src/POO/Guanabara/Aula05/ContaBanco.java
@@ -1,8 +1,8 @@
 package POO.Guanabara.Aula05;
 
 public class ContaBanco {
-    private double numConta;
-    private String tipo;
+    public double numConta;
+    protected String tipo;
     private String dono;
     private double saldo;
     private boolean status;
@@ -24,15 +24,65 @@ public class ContaBanco {
     }
 
     void depositar(){
+        this.status = true;
 
     }
 
     void sacar(){
-
+    this.status = true;
+    if(this.saldo > 0 && this.saldo <= 1.000){
+        System.out.println("Sacando...");
+    }
     }
 
     void pagarMensal(){
+        this.status = true;
+        if(this.tipo == "CC"){
+            double desconto = this.saldo-12;
+        }else{
+            double desconto = this.saldo-20;
+        }
 
+    }
+    // Getters and Setters:
+    public double getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(double numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
 
