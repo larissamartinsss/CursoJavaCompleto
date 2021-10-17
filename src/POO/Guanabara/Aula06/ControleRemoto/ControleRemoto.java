4f0f6f23abdcbb5530f4aba77361ee554a4fd08e
Larissa Martins <larissamartins.job@gmail.com>
2021-10-17 17:12:23 -0400

    atividade concluída.

--- src/POO/Guanabara/Aula06/ControleRemoto/ControleRemoto.java
+++ src/POO/Guanabara/Aula06/ControleRemoto/ControleRemoto.java
@@ -11,77 +11,93 @@ public class ControleRemoto implements Controlador {
         this.tocando = false;
     }
 
-    public int getVolume() {
+    private int getVolume() {
         return this.volume;
     }
 
-    public void setVolume(int volume) {
+    private void setVolume(int volume) {
         this.volume = volume;
     }
 
-    public boolean isLigado() {
+    private boolean isLigado() {
         return this.ligado;
     }
 
-    public void setLigado(boolean ligado) {
+    private void setLigado(boolean ligado) {
         this.ligado = ligado;
     }
 
-    public boolean isTocando() {
+    private boolean isTocando() {
         return this.tocando;
     }
 
-    public void setTocando(boolean tocando) {
+    private void setTocando(boolean tocando) {
         this.tocando = tocando;
     }
 
     @Override
     public void ligar() {
-
+        this.setLigado(true);
     }
 
     @Override
     public void desligar() {
-
+        this.setLigado(false);
     }
 
     @Override
     public void abrirMenu() {
-
+        System.out.println("-------- Menu --------");
+        System.out.println("Está ligado? " +this.isLigado());
+        System.out.println("Está tocando " + this.isTocando());
+        System.out.println("Volume: "+ this.getVolume());
+        for (int i = 0; i <=this.getVolume() ; i+=10) {
+            System.out.print("|");
+        }
     }
-
     @Override
     public void fecharMenu() {
-
+        System.out.println("Fechando menu ");
     }
 
     @Override
     public void maisVolume() {
-
+        if(isLigado()){
+            setVolume(getVolume()+5);
+        }
     }
 
     @Override
     public void menosVolume() {
-
+        if(isLigado()){
+            setVolume(getVolume()-5);
+        }
     }
 
     @Override
     public void ligarMudo() {
-
+        if(isLigado() && getVolume() > 0){
+            setVolume(0);
+        }
     }
-
     @Override
     public void desligarMudo() {
-
+        if(isLigado() && getVolume() == 0){
+            setVolume(50);
+        }
     }
 
     @Override
     public void play() {
-
+        if (this.isLigado() && !(this.isTocando())) {
+            this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
-
+        if(this.isLigado() && !(this.isTocando())){
+            this.setTocando(false);
+        }
     }
 }
---
+++ src/POO/Guanabara/Aula06/ControleRemoto/Main.java
@@ -0,0 +1,11 @@
+package POO.Guanabara.Aula06.ControleRemoto;
+
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
