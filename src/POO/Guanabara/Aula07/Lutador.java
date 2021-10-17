97bc5b515c6b30cfd8cb8fb17bcc6e081aeda37b
Larissa Martins <larissamartins.job@gmail.com>
2021-10-17 17:46:42 -0400

    Iniciando atividade 07

--- src/POO/Guanabara/Aula07/Lutador.java
+++ src/POO/Guanabara/Aula07/Lutador.java
@@ -11,6 +11,19 @@ public class Lutador {
     private int derrotas;
     private int empates;
 
+    public Lutador(String nome, String nacionalidade, int idade, double altura,
+                   double peso, int vitorias, int derrotas, int empates)
+    {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+
     public void apresentar(){
     }
 
