3a54684506915fedd9c9286261a5a6bc9cec3347
Larissa Martins <larissamartins.job@gmail.com>
2021-10-19 20:43:25 -0400

    Finalizando atividade

--- src/POO/Guanabara/Aula07/Luta.java
+++ src/POO/Guanabara/Aula07/Luta.java
@@ -1,16 +1,93 @@
 package POO.Guanabara.Aula07;
 
+import java.util.Random;
+
 public class Luta {
-    private String desafiado;
-    private String desafiante;
+    private Lutadora desafiada;
+    private Lutadora desafiante;
     private int rounds;
     private boolean aprovada;
 
-    public void marcarLuta(){
+    public void marcarLuta(Lutadora l1, Lutadora l2){
+        if(l1.getCategoria().equals(l2.getCategoria()) && l1 !=l2){
+            this.aprovada = true;
+            this.desafiada = l1;
+            this.desafiante = l2;
+        }else{
+            this.aprovada = false;
+            this.desafiada = null;
+            this.desafiante = null;
+        }
+
 
     }
-    public boolean lutar(){
-        return lutar();
+    public void lutar(){
+        if(isAprovada()){
+            System.out.println("###### DESAFIADA ######");
+            desafiada.apresentar();
+            System.out.println("###### DESAFIANTE ######");
+            desafiante.apresentar();
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); // 0 1 2
+            System.out.println("======== RESULTADO DA LUTA ========");
+            switch (vencedor) {
+                case 0: // empate
+                    System.out.println("Empatou!");
+                    desafiada.empatarLuta();
+                    desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println("Vitória da: "+desafiada.getNome());
+                    desafiada.ganharLuta();
+                    desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println("Derrota da: "+desafiante.getNome());
+                    desafiante.ganharLuta();
+                    desafiada.perderLuta();
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+
+        }else {
+            System.out.println("A luta não pode acontecer");
+        }
+
+
+    }
+
+    public Lutadora getDesafiada() {
+        return desafiada;
+    }
+
+    public void setDesafiada(Lutadora desafiada) {
+        this.desafiada = desafiada;
+    }
+
+    public Lutadora getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutadora desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
     }
 }
 
--- src/POO/Guanabara/Aula07/Main.java
+++ src/POO/Guanabara/Aula07/Main.java
@@ -6,21 +6,27 @@ public class Main {
         l[0] = new Lutadora("Cecilia", "Belgica", 31, 1.75,
                 68.9, 11, 2 ,1 );
         l[1] = new Lutadora("Kate", "Estados Unidos", 28, 1.80,
-                78.9, 9, 4 ,2 );
-        l[2] = new Lutadora("Paola", "México", 31, 1.75, 78.9, 9, 4 ,2 );
-        l[3] = new Lutadora("Jéssica", "Brasil", 26, 1.72, 78.9, 9, 4 ,2 );
-        l[4] = new Lutadora("Cecilia", "Holanda", 25,1.75, 78.9, 9, 4 ,2 );
+                67.9, 9, 4 ,2 );
+        l[2] = new Lutadora("Paola", "México", 31, 1.75, 76.9, 9, 4 ,2 );
+        l[3] = new Lutadora("Jéssica", "Brasil", 26, 1.72, 77.3, 9, 4 ,2 );
+        l[4] = new Lutadora("Cecilia", "Holanda", 25,1.75, 89.9, 9, 4 ,2 );
         l[5] = new Lutadora("Cecilia", "Chile", 31, 1.75, 78.9, 9, 4 ,2 );
-        l[6] = new Lutadora("Cecilia", "Africa do sul", 32, 1.81, 90.9, 11,2, 2);
+        l[6] = new Lutadora("Cecilia", "Africa do sul", 32, 1.81, 90.4, 11,2, 2);
 
 
+//        l[0].status();
+//        l[1].status();
+//        l[2].status();
+//        l[3].status();
+//        l[4].status();
+//        l[5].status();
+//        l[6].status();
+
+        Luta UFC01 = new Luta();
+        UFC01.marcarLuta(l[1],l[5] );
+        UFC01.lutar();
         l[0].status();
         l[1].status();
-        l[2].status();
-        l[3].status();
-        l[4].status();
-        l[5].status();
-        l[6].status();
 
 
     }
